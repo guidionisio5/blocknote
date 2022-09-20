839816ecc9b1e8c7224725a6be959aafe97b63eb
Guilherme Dionisio <101677648+guidionisio5@users.noreply.github.com>
2022-09-20 02:24:27 -0300

    login html

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -19,23 +19,27 @@
         <nav class="navbar navbar-dark bg-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.php">BlockNote</a>
-                <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                <div class="offcanvas offcanvas-end navbar-dark bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title text-light" id="offcanvasNavbarLabel">Opções</h5>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body text-end">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Suporte</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Link</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </nav>
-        <div class="collapse" id="navbarToggleExternalContent">
-            <div class="bg-dark text-bg-dark text-end p-4">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Suporte</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
         <!-- FIM MENU -->
 
         <!-- INÍCIO CADASTRO USUÁRIO -->
