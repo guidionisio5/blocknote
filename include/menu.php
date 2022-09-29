a2e3e521528a8da1ceaea217fc2d6572f70cace3
guidionisio5 <101677648+guidionisio5@users.noreply.github.com>
2022-09-28 22:34:11 -0300

    arrumando menu

--- include/menu.php
+++ include/menu.php
@@ -22,8 +22,8 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
