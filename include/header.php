6bf3572fb47248e2ea438629c07a0a1b90ae1d6b
Guilherme Dionisio <101677648+guidionisio5@users.noreply.github.com>
2023-01-27 03:40:37 -0300

    ajuste menu celular

--- include/header.php
+++ include/header.php
@@ -7,10 +7,10 @@ include '../backend/imagem_usuario.php';
 <nav class="navbar navbar-expand-lg header">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon navbar-dark"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+      <ul class="navbar-nav me-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
           <a class="nav-link texto-menu" href="paginaPrincipal.php">Home</a>
         </li>
@@ -24,7 +24,7 @@ include '../backend/imagem_usuario.php';
           <a class="nav-link texto-menu" href="lembretes.php">Lembretes</a>
         </li>
       </ul>
-        <div class="d-flex">
+        <div class="d-flex flex-row-reverse">
           <button type="button" class="btn btn-link nav-link py-2 px-0 px-lg-2" data-bs-toggle="dropdown" aria-expanded="false" data-bs-display="static">
             <img src="../page/assets/img/<?php if($imagem == null){
                 echo 'user.png';
